54b73d7dc93f0a6ba81c8336e9e343770dc3f513
BaiqingL <me@baiqinglyu.com>
2022-11-11 22:39:08 -0500

    [Fix] Change screen names and add default route

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Link, Router, Routes} from 'react-router-dom';
+import OpeningScreen from './screens/OpeningScreen';
+import AddressWarningScreen from './screens/AddressWarningScreen';
+
+import AddressEntryScreen from './screens/AddressEntryScreen';
+import LoginScreen from './screens/LoginScreen';
+import UserProfileScreen from './screens/UserProfileScreen';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginScreen/>} />
+          <Route path="/opening" element={<OpeningScreen/>} />
+          <Route path="/address-warning" element={<AddressWarningScreen/>} />
+          <Route path="/address-entry" element={<AddressEntryScreen/>} />
+          <Route path="/login" element={<LoginScreen/>} />
+          <Route path="/user-profile" element={<UserProfileScreen/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/AddressCheckBox.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+
+function AddressCheckBox () {
+  
+    return (
+      <div>
+        This is AddressCheckBox
+      </div>
+    );
+  
+}
+
+export default AddressCheckBox;
