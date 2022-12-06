915838fd4abb7b58ea2716d80cfc81750233b705
Baiqing Lyu <baiqing@bu.edu>
2022-12-05 19:07:51 -0500

    [Feat] Enable button highlighting

--- src/screens/ChooseInterestScreen.tsx
+++ src/screens/ChooseInterestScreen.tsx
@@ -83,11 +83,10 @@ function ChooseInterestScreen() {
         return (
           // Set style to have black text color
           <Chip 
-            className='px-3 m-1' 
+            className={interest.selected ? 'px-3 m-1 .selected-chip-clicked' : 'px-3 m-1 selected-chip-non-clicked'} 
             onClick={() => highlightItem(interest.id)} 
             isReadOnly 
             isOverflowChip
-            style={{ color: interest.selected ? "red" : "black" }}
             >
             {interest.name}
           </Chip>
