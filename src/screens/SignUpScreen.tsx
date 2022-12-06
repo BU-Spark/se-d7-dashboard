c15c971c2c84ce0e65ff38ad363a751378815b6f
Baiqing Lyu <baiqing@bu.edu>
2022-12-05 23:46:52 -0500

    [Chore] Formatting

--- src/screens/SignUpScreen.tsx
+++ src/screens/SignUpScreen.tsx
@@ -1,6 +1,6 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 // import firebase auth
-import {getAuth, createUserWithEmailAndPassword} from "firebase/auth"
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import { Button, Alert } from "@patternfly/react-core";
 import "bootstrap/dist/css/bootstrap.css";
@@ -14,84 +14,79 @@ const SignUpScreen: React.FunctionComponent<ILoginScreenProps> = (props) => {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [isBannerVisible, setIsBannerVisible] = useState(false);
-  const [bannerMessage, setBannerMessage] = useState('This is a banner.');
+  const [bannerMessage, setBannerMessage] = useState("This is a banner.");
 
   const navigateToNext = () => {
-    navigate('/choose-interest');
+    navigate("/choose-interest");
   };
 
   const SignUp = async () => {
     const user = {
       email,
       password,
-    }
+    };
 
     console.log(user);
 
-
     createUserWithEmailAndPassword(auth, email, password)
-    .then((response) => {
-      console.log(response.user.uid);
-      navigate("/choose-interest");
-    })
-    .catch((error) => {
-      setIsBannerVisible(true);
-      // Only get the message in the parathesis
-      let parsedMessage = error.message.match(/\(([^)]+)\)/)[1];
-      // Match the message to the error message
-      if (parsedMessage === 'auth/email-already-in-use') {
-        setBannerMessage('Email already in use.');
-      } else if (parsedMessage === 'auth/invalid-email') {
-        setBannerMessage('Invalid email.');
-      } else if (parsedMessage === 'auth/weak-password') {
-        setBannerMessage('Password is too weak.');
-      } else {
-        setBannerMessage('Something went wrong.');
-      }
-      console.log(error);
-      setAuthing(false);
-    });
-  }
+      .then((response) => {
+        console.log(response.user.uid);
+        navigate("/choose-interest");
+      })
+      .catch((error) => {
+        setIsBannerVisible(true);
+        // Only get the message in the parathesis
+        let parsedMessage = error.message.match(/\(([^)]+)\)/)[1];
+        // Match the message to the error message
+        if (parsedMessage === "auth/email-already-in-use") {
+          setBannerMessage("Email already in use.");
+        } else if (parsedMessage === "auth/invalid-email") {
+          setBannerMessage("Invalid email.");
+        } else if (parsedMessage === "auth/weak-password") {
+          setBannerMessage("Password is too weak.");
+        } else {
+          setBannerMessage("Something went wrong.");
+        }
+        console.log(error);
+        setAuthing(false);
+      });
+  };
 
   return (
     <div className="container">
-              <div className="mb-3 pf-c-title h4 text-start">Sign Up</div>
-              <div className="text-start">Email</div>
-      
+      <div className="mb-3 pf-c-title h4 text-start">Sign Up</div>
+      <div className="text-start">Email</div>
+
       <input
-            value={email}
-            onChange={(e) => {
-              setEmail(e.target.value);
-            }}
-            type="text"
-            className="input-text mb-3"
-            placeholder=""
-          />
-          <div className="text-start">Password</div>
-          <input
-            value={password}
-            onChange={(e) => {
-              setPassword(e.target.value);
-            }}
-            type="password"
-            className="input-text mb-3"
-            placeholder=""
-          />
+        value={email}
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+        type="text"
+        className="input-text mb-3"
+        placeholder=""
+      />
+      <div className="text-start">Password</div>
+      <input
+        value={password}
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+        type="password"
+        className="input-text mb-3"
+        placeholder=""
+      />
       <br />
-      {isBannerVisible && 
-      (<Alert variant="danger" title={bannerMessage} />)}
+      {isBannerVisible && <Alert variant="danger" title={bannerMessage} />}
       <br />
 
-        <div className="text-end mt-5">
+      <div className="text-end mt-5">
         <Button onClick={SignUp} className="px-3 py-1" variant="primary">
           Next
         </Button>
       </div>
-
-
-      
     </div>
   );
-}
+};
 
 export default SignUpScreen;
