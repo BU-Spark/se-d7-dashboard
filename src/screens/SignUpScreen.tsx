0ac50b61b514bfa9c6512677b406cafbf572e393
Baiqing Lyu <baiqing@bu.edu>
2022-12-05 23:26:14 -0500

    [Feat] Show error banner when login or signup instead of alerts

--- src/screens/SignUpScreen.tsx
+++ src/screens/SignUpScreen.tsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 // import firebase auth
 import {getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword} from "firebase/auth"
 import { useNavigate } from "react-router-dom";
-import { Checkbox, TextInput, Button,Chip } from "@patternfly/react-core";
+import { Checkbox, TextInput, Button, Banner } from "@patternfly/react-core";
 import "bootstrap/dist/css/bootstrap.css";
 
 export interface ILoginScreenProps {}
@@ -13,32 +13,12 @@ const SignUpScreen: React.FunctionComponent<ILoginScreenProps> = (props) => {
   const [authing, setAuthing] = useState(false);
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const [isBannerVisible, setIsBannerVisible] = useState(false);
+  const [bannerMessage, setBannerMessage] = useState('This is a banner.');
 
   const navigateToNext = () => {
     navigate('/choose-interest');
   };
-
-  const Login = async () => {
-    const user = {
-      email,
-      password,
-    }
-    setAuthing(true);
-
-    signInWithEmailAndPassword(auth, user.email, user.password)
-        .then((response) => {
-            console.log(response.user.uid);
-            navigate('/user-profile');
-        })
-        .catch((error) => {
-            alert(error);
-            setAuthing(false);
-        });
-};
-
-// const navigateToSignUp = () => {
-//   navigate('/address-warning');
-// };
 
   const SignUp = async () => {
     const user = {
@@ -48,16 +28,19 @@ const SignUpScreen: React.FunctionComponent<ILoginScreenProps> = (props) => {
 
     console.log(user);
 
-    try {
-      await createUserWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
-      navigate('/choose-interest');
-    } catch (error) {
-      alert(error);
-    }
+
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((response) => {
+      console.log(response.user.uid);
+      navigate("/choose-interest");
+    })
+    .catch((error) => {
+      setIsBannerVisible(true);
+      // Only get the message in the parathesis
+      setBannerMessage(error.message.match(/\(([^)]+)\)/)[1]);
+      console.log(error);
+      setAuthing(false);
+    });
   }
 
   return (
@@ -84,23 +67,17 @@ const SignUpScreen: React.FunctionComponent<ILoginScreenProps> = (props) => {
             className="input-text mb-3"
             placeholder=""
           />
-
-{/* <div className="text-start"> Re-enter Password</div>
-          <input
-            value={password}
-            onChange={(e) => {
-              setPassword(e.target.value);
-            }}
-            type="password"
-            className="input-text mb-3"
-            placeholder=""
-          /> */}
-
-
-        
-        {/* <Button className="px-5 py-1" style={{width:"260px"}} variant="secondary" onClick={SignUp} >
-        Sign up
-        </Button> */}
+      <br />
+      {isBannerVisible && (
+        <Banner
+          variant="danger"
+          title="Error"
+          className="mb-3"
+          aria-live="polite"
+        >
+          {bannerMessage}
+        </Banner>
+      )}
 
         <div className="text-end mt-5">
         <Button onClick={SignUp} className="px-3 py-1" variant="primary">
