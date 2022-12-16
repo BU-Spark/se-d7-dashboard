1a9aff639f9f4d7afe486d50e78099874b7d749d
Yichen Liu <kittyliuyichen@163.com>
2022-12-16 14:55:03 -0500

    calendar display, still need date working

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,5 +1,10 @@
 import * as React from "react";
-import { Card, CardBody, Text, Icon } from "@patternfly/react-core";
+
+import {
+  Button,
+  SearchInput,
+  
+} from "@patternfly/react-core";
 import "@patternfly/react-core/dist/styles/base.css";
 import "bootstrap/dist/css/bootstrap.css";
 import { EllipsisVIcon } from "@patternfly/react-icons";
@@ -15,15 +20,144 @@ import {
   addWeeks,
   subWeeks
 } from "date-fns";
+import { useState } from "react";
+
 
 function Calendar() {
+  const [currentMonth, setCurrentMonth] = useState(new Date());
+  const [currentWeek, setCurrentWeek] = useState(getWeek(currentMonth));
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  const changeMonthHandle = (btnType: string) => {
+    if (btnType === "prev") {
+      setCurrentMonth(subMonths(currentMonth, 1));
+    }
+    if (btnType === "next") {
+      setCurrentMonth(addMonths(currentMonth, 1));
+    }
+  };
+
+  const changeWeekHandle = (btnType: string) => {
+    //console.log("current week", currentWeek);
+    if (btnType === "prev") {
+      //console.log(subWeeks(currentMonth, 1));
+      setCurrentMonth(subWeeks(currentMonth, 1));
+      setCurrentWeek(getWeek(subWeeks(currentMonth, 1)));
+    }
+    if (btnType === "next") {
+      //console.log(addWeeks(currentMonth, 1));
+      setCurrentMonth(addWeeks(currentMonth, 1));
+      setCurrentWeek(getWeek(addWeeks(currentMonth, 1)));
+    }
+  };
+  const onDateClickHandle = (day:Date, dayStr:string) => {
+    setSelectedDate(day);
+  };
+
+  const renderHeader = () => {
+    const dateFormat = "MMM yyyy";
+    // console.log("selected day", selectedDate);
+    return (
+      <div className="header row flex-middle">
+        <div className="col col-start">
+          {/* <div className="icon" onClick={() => changeMonthHandle("prev")}>
+            prev month
+          </div> */}
+        </div>
+        <div className="col col-center">
+          <span>{format(currentMonth, dateFormat)}</span>
+        </div>
+        <div className="col col-end">
+          {/* <div className="icon" onClick={() => changeMonthHandle("next")}>next month</div> */}
+        </div>
+      </div>
+    );
+  };
+  const renderDays = () => {
+    const dateFormat = "EEE";
+    const days = [];
+    let startDate = startOfWeek(currentMonth, { weekStartsOn: 1 });
+    for (let i = 0; i < 7; i++) {
+      days.push(
+        <div className="col col-center" key={i}>
+          {/* {format(addDays(startDate, i), dateFormat)} */}
+        </div>
+      );
+    }
+    return <div className="days row">{days}</div>;
+  };
 
 
+  const renderCells = () => {
+    const startDate = startOfWeek(currentMonth, { weekStartsOn: 1 });
+    const endDate = lastDayOfWeek(currentMonth, { weekStartsOn: 1 });
+    const dateFormat = "d";
+    const rows = [];
+    let days = [];
+    let day = startDate;
+    let formattedDate = "";
+    while (day <= endDate) {
+      for (let i = 0; i < 7; i++) {
+        formattedDate = format(day, dateFormat);
+        
+        const cloneDay = day;
+        days.push(
+          <div
+            className={`col cell ${
+              isSameDay(day, new Date())
+                ? "today"
+                : isSameDay(day, selectedDate)
+                ? "selected"
+                : ""
+            }`}
+            // key={}
+            onClick={() => {
+              const dayStr = format(cloneDay, "ccc dd MMM yy");
+              onDateClickHandle(cloneDay, dayStr);
+            }}
+          >
+            <span className="number">{formattedDate}</span>
+            <span className="bg">{formattedDate}</span>
+          </div>
+        );
+        day = addDays(day, 1);
+      }
+
+      rows.push(
+        <div className="row" key={formattedDate}>
+          {days}
+        </div>
+      );
+      days = [];
+    }
+    return <div className="body">{rows}</div>;
+  };
+  const renderFooter = () => {
+    return (
+      <div className="header row flex-middle">
+        <div className="col col-start">
+          <Button className="px-2 py-1 mb-2" onClick={() => changeWeekHandle("prev")}>
+            prev week
+          </Button>
+        </div>
+        {/* <div>{currentWeek}</div> */}
+        <div className="col col-end" onClick={() => changeWeekHandle("next")}>
+          <Button className="px-2 py-1 mb-2">next week</Button>
+        </div>
+      </div>
+    );
+  };
   return (
-    <div >
-      this is the calendar component
+    <div className="calendar">
+      {renderHeader()}
+      {renderDays()}
+      {renderCells()}
+      {renderFooter()}
     </div>
   );
-}
+};
+
+
+
 
 export default Calendar;
