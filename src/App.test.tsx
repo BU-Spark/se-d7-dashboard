c7c3817e193046c3701f98fa0b4ab7501945696f
Baiqing Lyu <baiqinglyu@gmail.com>
2022-12-19 18:14:08 -0500

    Render base app without warnings

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('Placeholder Test', () => {
-  console.log("Placeholder Test")
+test('App init test', () => {
+  const { getByText } = render(<App />);
 });
