3cc7910e00af2c5769e683deab28f84d52cf26e4
Arsamat <67019552+Arsamat@users.noreply.github.com>
2023-04-05 01:32:40 -0400

    strapi internal server error fix

--- strapi/config/env/development/database.js
+++ strapi/config/env/development/database.js
@@ -1,13 +1,26 @@
 module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
     connection: {
-      client: 'postgres',
-      connection: {
-        host: env('PGHOST', '127.0.0.1'),
-        port: env.int('PGPORT', 6171),
-        database: env('PGDATABASE', 'railway'),
-        user: env('PGUSER', 'postgres'),
-        password: env('PGPASSWORD', 'rkQpi5ikkGw0z3CHlkho'),
-        ssl: env.bool(true),
-      },
+      host: env('PGHOST', '127.0.0.1'),
+      port: env.int('PGPORT', 6171),
+      database: env('PGDATABASE', 'railway'),
+      user: env('PGUSER', 'postgres'),
+      password: env('PGPASSWORD', 'rkQpi5ikkGw0z3CHlkho'),
+      ssl: env.bool(true),
     },
-  });
+    debug: false,
+    acquireConnectionTimeout: 600000,
+    pool: {
+      min: 0,
+      max: 100,
+      acquireTimeoutMillis: 300000,
+      createTimeoutMillis: 300000,
+      destroyTimeoutMillis: 50000,
+      idleTimeoutMillis: 300000,
+      reapIntervalMillis: 10000,
+      createRetryIntervalMillis: 2000,
+      propagateCreateError: false,
+    },
+  },
+});
