db4b54fbcc1ec0eb18cc8cf668b3a8792cfc585d
kaquoc <kaquoc1206@gmail.com>
2023-04-11 14:21:11 -0400

    added existing resources to Strapi

---
+++ strapi/src/api/submit-requests-and-report/controllers/submit-requests-and-report.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * submit-requests-and-report controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::submit-requests-and-report.submit-requests-and-report');
---
+++ strapi/src/api/submit-requests-and-report/services/submit-requests-and-report.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * submit-requests-and-report service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::submit-requests-and-report.submit-requests-and-report');
