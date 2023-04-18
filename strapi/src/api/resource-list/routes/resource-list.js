854c84177037f7fe06a8f31fd1dbcb60aea835d2
kaquoc <kaquoc1206@gmail.com>
2023-04-18 17:02:50 -0400

    add resource_list to strapi

---
+++ strapi/src/api/resource-list/routes/resource-list.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * resource-list router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::resource-list.resource-list');
