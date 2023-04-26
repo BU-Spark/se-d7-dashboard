aaffcf60495a6cd3935ab0831652bca7d84086d5
Arsamat <67019552+Arsamat@users.noreply.github.com>
2023-04-26 02:15:17 -0400

    Added sign out button

---
+++ src/screens/LogOut.tsx
@@ -0,0 +1,28 @@
+import { getAuth, signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { Button } from "@patternfly/react-core";
+
+
+function LogOut() {
+    const navigate = useNavigate();
+
+    const auth = getAuth();
+    const SignOut = () => {
+        signOut(auth).then(() => {
+            // Sign-out successful.
+            console.log("Logged out")
+            navigate("/login")
+        }).catch((error) => {
+            // An error happened.
+            console.log(error)
+        });
+    }
+
+    return (
+        <div className="logout-button-div">
+            <button onClick={SignOut} className="logout-button">Sign Out</button>
+        </div>
+    )
+}
+
+export default LogOut
