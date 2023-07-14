51e91c754db2a2fc944c0749caeb714c3c4e7905
christorange <christol.yu@outlook.com>
2023-07-13 22:11:57 -0400

    style(welcome): fix button style

--- src/screens/welcome/Welcome.tsx
+++ src/screens/welcome/Welcome.tsx
@@ -41,10 +41,10 @@ function Welcome() {
           <div className="welcome_title">Welcome to the District 7 Boston Citizen’s App</div>
           <div className="bottom_section">
             <div className="mt-5 mb-4 welcome_hint"> Please confirm you are a resident of District 7 </div>
-            <Button onClick={navigateToNext} className="px-5 py-1 mb-4 welcome_button"variant="primary">
+            <Button onClick={navigateToNext} className="welcome_button"variant="primary">
               I am a resident of D7
             </Button>
-            <Button onClick={navigateToSignUp} className="px-5 py-1 welcome_button" variant="secondary">
+            <Button onClick={navigateToSignUp} className="welcome_button" variant="secondary">
               Find out
             </Button>
           </div>  
