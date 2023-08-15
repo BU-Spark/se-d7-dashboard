7512018339acb211a5d169779569cfe5587bd3ed
Jason Oh <jasonoh1998@gmail.com>
2023-08-15 13:57:59 -0400

    posts page

--- src/screens/AllPosts.tsx
+++ src/screens/AllPosts.tsx
@@ -1,14 +1,15 @@
 import React, { useEffect } from "react";
-import { AngleLeftIcon } from "@patternfly/react-icons";
+import { SearchIcon, AngleLeftIcon } from "@patternfly/react-icons";
 import { useNavigate } from "react-router-dom";
-import type { upData } from "./Home";
+import type { postData } from "./Home";
 import { APIUrl } from "./Home";
-import Updates from "../components/home/Updates";
+import PostCards from "../components/home/posts/PostCards";
 
 function AllPosts() {
   const navigate = useNavigate();
 
-  const [updates, setUpdates] = React.useState<upData[]>([]);
+  const [updates, setUpdates] = React.useState<postData[]>([]);
+  const [searchTerm, setSearchTerm] = React.useState('');
 
   useEffect(() => {
     const fetchUpdates = async () => {
@@ -26,6 +27,7 @@ function AllPosts() {
                 attributes: {
                   title: "Uh Oh!",
                   content: "Looks like there was an issue!",
+                  createdAt: "2023-04-26T06:23:31.752Z",
                 },
               },
             ]);
@@ -37,14 +39,36 @@ function AllPosts() {
     };
     fetchUpdates();
   }, []);
+  
+
+  const handleSearch = (e: any) => {
+    setSearchTerm(e.target.value.toLowerCase());
+  };
+
+  // filter to lowercase
+  const filteredUpdates = updates.filter((update) => {
+    const title = update.attributes.title.toLowerCase();
+    const content = update.attributes.content.toLowerCase();
+    // finds similar content in both title and content
+    return title.includes(searchTerm) || content.includes(searchTerm);
+  });
 
   return (
-    <div>
-      <div className="mt-4 ms-4 portal-nav">
+    <div className="bg">
+      <div className="mt-3 ms-3 portal-nav-custom">
         <AngleLeftIcon size="md" onClick={() => navigate("/home")} />
-        All Posts
+        &nbsp;&nbsp;Posts
       </div>
-      <Updates updates={updates} vertical={true} />
+      <div className="search-container">
+        {!searchTerm.length && <SearchIcon className="search-icon" />}
+        <input 
+          type="text" 
+          placeholder="        Search Posts" 
+          onChange={handleSearch}
+          className="search-bar"
+        />
+      </div>
+      <PostCards updates={filteredUpdates} vertical={true} />
     </div>
   );
 }
