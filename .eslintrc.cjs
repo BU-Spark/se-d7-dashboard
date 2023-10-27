a0cddcaf170ca8e5862281e449439651bdb2295c
christorange <christol.yu@outlook.com>
2023-10-27 18:18:42 -0400

    chore: update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -26,12 +26,13 @@ const config = {
     '@typescript-eslint/no-non-null-assertion': 'warn',
     "@typescript-eslint/no-unused-vars": "off",
     "no-unused-vars": "off",
-    "unused-imports/no-unused-imports": "warn",
+    "unused-imports/no-unused-imports": "error",
     "unused-imports/no-unused-vars": [
       "warn",
       { "vars": "all", "varsIgnorePattern": "^_", "args": "after-used", "argsIgnorePattern": "^_" }
     ],
     "@typescript-eslint/no-empty-interface": "warn",
+    "@typescript-eslint/no-explicit-any": "warn"
 
 
   },
